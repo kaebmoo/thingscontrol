4cb03d1181a7b2c16349d4a594f1a1c7b104e8f8
kaebmoo <kaebmoo@gmail.com>
2017-06-23 19:38:47 +0700

    set 8 profiles timer

---
+++ read.netpie.8profile.php
@@ -0,0 +1,293 @@
+<?php
+/*
+    Things Control. Control anything as you want.
+    Copyright (C) 2017  Pornthep Nivatyakul
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    ThingsControl  Copyright (C) 2017  Pornthep Nivatyakul, kaebmoo@gmail.com, seal@ogonan.com
+    This program comes with ABSOLUTELY NO WARRANTY;
+    This is free software, and you are welcome to redistribute it
+    under certain conditions.
+
+    run every minute
+    * * * * * /home/pi/thingscontrol/getprofile.sh
+
+*/
+        //include('./httpful.phar');
+
+  include "httpful.phar";
+  /*
+	$uri = "https://api.netpie.io/topic";
+	$app = "/ThingsControl";
+	$topic = "/seal";
+	$appkey = "key";
+	$appsecret = "secret";
+  */
+
+  // Parse without sections
+  $ini_array = parse_ini_file("thingscontrol.conf");
+  $uri = $ini_array['uri'];
+  $app = $ini_array['app_id'];
+  $topic = $ini_array['id'];
+  $profile_uri = $ini_array['8profile'];
+  $param = $ini_array['param'];
+  $appkey = $ini_array['key'];
+  $appsecret = $ini_array['secret'];
+
+  //$uri_ = $uri . $app . $topic . $profile . $param . $key . ":" . $secret ;
+
+
+	$uri_start = $uri . $app  . $topic;
+	$uri_end = $appkey . ":" . $appsecret;
+
+  $uri_profile = $uri_start . $profile_uri . $param . $uri_end;
+  //echo $uri_profile . "\n";
+
+  $fp = fsockopen("udp://8.8.8.8", 53, $errno, $errstr);
+  if (!$fp) {
+    echo "ERROR: $errno - $errstr<br />\n";
+    exit(1);
+  }
+
+  $profile0 = "";
+  $profile = "";
+  $profile8 = array
+    (
+    array("Sun"=>"F", "Mon"=>"F", "Tue"=>"F", "Wed"=>"F", "Thu"=>"F", "Fri"=>"F", "Sat"=>"F", "Enable"=>"F", "HH"=>"00", "MM"=>"00", "OnTimer"=>"00"),
+    array("Sun"=>"F", "Mon"=>"F", "Tue"=>"F", "Wed"=>"F", "Thu"=>"F", "Fri"=>"F", "Sat"=>"F", "Enable"=>"F", "HH"=>"00", "MM"=>"00", "OnTimer"=>"00"),
+    array("Sun"=>"F", "Mon"=>"F", "Tue"=>"F", "Wed"=>"F", "Thu"=>"F", "Fri"=>"F", "Sat"=>"F", "Enable"=>"F", "HH"=>"00", "MM"=>"00", "OnTimer"=>"00"),
+    array("Sun"=>"F", "Mon"=>"F", "Tue"=>"F", "Wed"=>"F", "Thu"=>"F", "Fri"=>"F", "Sat"=>"F", "Enable"=>"F", "HH"=>"00", "MM"=>"00", "OnTimer"=>"00"),
+    array("Sun"=>"F", "Mon"=>"F", "Tue"=>"F", "Wed"=>"F", "Thu"=>"F", "Fri"=>"F", "Sat"=>"F", "Enable"=>"F", "HH"=>"00", "MM"=>"00", "OnTimer"=>"00"),
+    array("Sun"=>"F", "Mon"=>"F", "Tue"=>"F", "Wed"=>"F", "Thu"=>"F", "Fri"=>"F", "Sat"=>"F", "Enable"=>"F", "HH"=>"00", "MM"=>"00", "OnTimer"=>"00"),
+    array("Sun"=>"F", "Mon"=>"F", "Tue"=>"F", "Wed"=>"F", "Thu"=>"F", "Fri"=>"F", "Sat"=>"F", "Enable"=>"F", "HH"=>"00", "MM"=>"00", "OnTimer"=>"00"),
+    array("Sun"=>"F", "Mon"=>"F", "Tue"=>"F", "Wed"=>"F", "Thu"=>"F", "Fri"=>"F", "Sat"=>"F", "Enable"=>"F", "HH"=>"00", "MM"=>"00", "OnTimer"=>"00"),
+    );
+  $var_name = array("Sun","Mon","Tue","Wed","Thu","Fri","Sat","Enable","HH", "MM", "OnTimer");
+
+  for (;;) {
+  		echo "weekday " . date('w') . "\n";
+  		$dayofweek = date('w');
+
+      $isProfileUpdated = 0;
+
+      /*
+      $HH = 0;
+  		$MM = 0;
+  		$HH2 = 0;
+  		$MM2 = 0;
+  		$HH3 = 0;
+  		$MM3 = 0;
+  		$Enable = "false";
+  		$Enable2 = "false";
+  		$Enable3 = "false";
+  		$OnTimer = 5;
+      */
+
+      // GET topic from netpie read "8profile"
+      $try = 0;
+      do {
+        if ($try > 2) {
+          echo "\n" . "Error code = " . json_decode($response->code, true);
+          exit(2);
+        }
+        try {
+          $response = \Httpful\Request::get($uri_profile)->send();
+        }
+        catch(Exception $e) {
+          echo 'Message: ' . $e->getMessage();
+          exit(1);
+        }
+        echo ".";
+        sleep(1);
+        $try++;
+
+      } while (json_decode($response->code, true) != 200);
+
+
+      $result = json_decode($response->body, true);
+      $profile = $result[0]['payload'];
+      $profile_lastUpdated = date('r', $result[0]['lastUpdated']);
+      echo "\n";
+      echo $profile . "\n";
+      echo $profile_lastUpdated . "\n";
+
+      if(strcmp($profile, $profile0) == 0) {
+        $isProfileUpdated = 0;
+        echo "Profile does not change.\n";
+      }
+      else {  // profile changed.
+        $profile0 = $profile;
+        $isProfileUpdated = 1;
+      }
+
+      if ($isProfileUpdated == 1) {
+        $extractProfile = array("","","","","","","","");
+
+        for ($row=0; $row < 8; $row++) {
+          // 11 variables 14 characters 8 profiles.
+          $extractProfile[$row] = substr($profile,$row*14,14);
+          $index = 0;
+          foreach($var_name as $VariableName) {
+            if ($index < 8) {
+          	 $GLOBALS[$VariableName] = substr($extractProfile[$row],$index++,1);
+             $profile8[$row][$VariableName] = $GLOBALS[$VariableName];
+            }
+            else {
+              $GLOBALS[$VariableName] = substr($extractProfile[$row],$index,2);
+              $index = $index + 2;
+              $profile8[$row][$VariableName] = $GLOBALS[$VariableName];
+            }
+          }
+        }
+
+        foreach ($profile8 as $key) {
+          foreach($var_name as $val)
+            echo $key[$val] . " ";
+          echo "\n";
+        }
+
+        /*
+        $HH = substr($profile, 10, 2);
+        $MM = substr($profile, 12, 2);
+        $HH2 = substr($profile, 14, 2);
+        $MM2 = substr($profile, 16, 2);
+	      $HH3 = substr($profile, 18, 2);
+        $MM3 = substr($profile, 20, 2);
+        */
+
+        /*
+        if (strcmp(substr($profile,7,1), "T") == 0) {
+          $Enable = "true";
+        }
+        else {
+          $Enable = "false";
+        }
+        if (strcmp(substr($profile,8,1), "T") == 0) {
+          $Enable2 = "true";
+        }
+        else {
+          $Enable2 = "false";
+        }
+        if (strcmp(substr($profile,9,1), "T") == 0) {
+          $Enable3 = "true";
+        }
+        else {
+          $HH3 = substr($profile, 18, 2);
+          $Enable3 = "false";
+        }
+        $OnTimer = substr($profile,22,2);
+
+        echo $HH . ":" . $MM . " " . $HH2 . ":" . $MM2 . " " . $HH3 . ":" . $MM3 . "\n";
+        echo $Enable . " " . $Enable2 . " " . $Enable3 . "\n";
+        echo $OnTimer . "\n";
+
+
+    		$Sun = "false";
+    		$Mon = "false";
+    		$Tue = "false";
+    		$Wed = "false";
+    		$Thu = "false";
+    		$Fri = "false";
+    		$Sat = "false";
+
+    		$var_weekday = array($Sun,$Mon,$Tue,$Wed,$Thu,$Fri,$Sat);
+
+
+        for ($i = 0; $i < 7; $i++)
+        {
+            if (strcmp(substr($profile, $i, 1), "F") == 0) {
+              $var_weekday[$i] = "false";
+            }
+            else {
+              $var_weekday[$i] = substr($profile, $i, 1);
+            }
+            //echo $var_weekday[$i] . "\n";
+        }
+
+    		$set_weekday = "";
+    		for ($i = 0; $i < 7; $i++) {
+    			if ($var_weekday[$i] != "false") {
+    					$set_weekday = $set_weekday . $var_weekday[$i] . ",";
+    			}
+    		}
+
+    		if ($set_weekday == "") {
+    			// user enable start time but all weekday is disabled.
+    			echo "All weekday is disabled.\n";
+    		}
+    		else {
+    			$time1 = "";
+    			$time2 = "";
+    			$time3 = "";
+
+    			//delete "," at the end of string.
+    			$set_weekday = substr($set_weekday, 0, strlen($set_weekday)-1) . "\t";
+    			// if start time is enalbed. we set the weekday in crontab.
+
+    			if ($Enable == "true") {
+    				$cron_weekday = "";
+    				//echo $MM . " " . $HH . " * * ";
+    				$cron_mm_hh = $MM . " " . $HH . " * * ";
+    				$cron_weekday = $cron_mm_hh . $set_weekday;
+    				echo $cron_weekday . "/home/pi/thingscontrol/bin/thingsontimer " . $GLOBALS["OnTimer"] . " >> /var/log/thingsontimer.log 2>&1\n";
+    				$time1 = $cron_weekday . "/home/pi/thingscontrol/bin/thingsontimer " . $GLOBALS["OnTimer"] . " >> /var/log/thingsontimer.log 2>&1\n";
+
+    			}
+
+    			if ($Enable2 == "true") {
+    				$cron_weekday = "";
+    				$cron_mm2_hh2 = $MM2 . " " . $HH2 . " * * ";
+    				$cron_weekday = $cron_mm2_hh2 . $set_weekday;
+    				echo $cron_weekday . "/home/pi/thingscontrol/bin/thingsontimer " . $GLOBALS["OnTimer"] . " >> /var/log/thingsontimer.log 2>&1\n";
+    				$time2 = $cron_weekday . "/home/pi/thingscontrol/bin/thingsontimer " . $GLOBALS["OnTimer"] . " >> /var/log/thingsontimer.log 2>&1\n";
+
+    			}
+    			if ($Enable3 == "true") {
+    				$cron_weekday = "";
+    				$cron_mm3_hh3 = $MM3 . " " . $HH3 . " * * ";
+    				$cron_weekday = $cron_mm3_hh3 . $set_weekday;
+    				echo $cron_weekday . "/home/pi/thingscontrol/bin/thingsontimer " . $GLOBALS["OnTimer"] . " >> /var/log/thingsontimer.log 2>&1\n";
+    				$time3 = $cron_weekday . "/home/pi/thingscontrol/bin/thingsontimer " . $GLOBALS["OnTimer"] . " >> /var/log/thingsontimer.log 2>&1\n";
+
+    			}
+
+          if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+
+          }
+          else {
+
+      			$out = shell_exec("/usr/bin/crontab -r");
+      			echo $out;
+      			$cron1 = "(crontab -l 2>/dev/null; echo \"" . $time1 . "\") | crontab - ";
+      			$cron2 = "(crontab -l 2>/dev/null; echo \"" . $time2 . "\") | crontab - ";
+      			$cron3 = "(crontab -l 2>/dev/null; echo \"" . $time3 . "\") | crontab - ";
+      			$out = shell_exec($cron1);
+      			echo $out;
+      			$out = shell_exec($cron2);
+      			echo $out;
+      			$out = shell_exec($cron3);
+      			echo $out;
+          }
+
+    		} // weekday enable
+        */
+        // (crontab -l 2>/dev/null; echo "*/5 * * * * /path/to/job -with args") | crontab -
+
+      } // check isProfileUpdated
+      sleep(5);
+
+  }
+
+?>
