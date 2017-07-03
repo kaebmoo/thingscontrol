0a694cac9aec64ee43f4a8cfa6fdbf509b045153
kaebmoo <kaebmoo@gmail.com>
2017-07-03 09:57:28 +0700

    fix

--- src/m.sh
+++ src/m.sh
@@ -1,3 +1,4 @@
 cc -lwiringPi -lcurl -lconfig -ljson-c thingson.c status-update.c -o ../bin/thingson
+cc -lwiringPi -lcurl -lconfig -ljson-c thingsontimer.c status-update.c -o ../bin/thingsontimer
 
 cc -lwiringPi -lcurl -lconfig -ljson-c update.netpie.status.c -o ../bin/update.netpie.status
