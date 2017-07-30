2dc6535abffbf7eb2e44f6789b6b3d99ac4eac03
kaebmoo <kaebmoo@gmail.com>
2017-07-30 18:58:39 +0700

    update compile shell script

--- src/compile.sh
+++ src/compile.sh
@@ -1,4 +1,5 @@
 cc -lwiringPi -lcurl -lconfig -ljson-c thingson.c status-update.c -o ../bin/thingson
+cc -lwiringPi -lcurl -lconfig -ljson-c thingsoff.c status-update.c -o ../bin/thingsoff
+cc -lwiringPi -lcurl -lconfig -ljson-c thingsonoff.c status-update.c -o ../bin/thingsonoff
 cc -lwiringPi -lcurl -lconfig -ljson-c thingsontimer.c status-update.c -o ../bin/thingsontimer
-
 cc -lwiringPi -lcurl -lconfig -ljson-c update.netpie.status.c -o ../bin/update.netpie.status
